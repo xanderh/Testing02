c55a44645ccdc0e810af78e057677536ec0f79ea
Biest1155 <mikk934a@edu.eal.dk>
2018-03-23 09:25:00 +0100

    Added Saveperson test

---
+++ UnitTestProject1/MyDataProvider.cs
@@ -0,0 +1,44 @@
+﻿using ApplicationUsingDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    class MyDataProvider : IDataProvider
+    {
+        List<Person> people = new List<Person>();
+        public int CountPeople()
+        {
+            return people.Count;
+
+        }
+
+        public bool ExistsPerson(Person pers)
+        {
+            return people.Exists((person)=> person.Equals(pers));
+        }
+
+        public ICollection<Payment> GetPaymentsFromPerson(Person pers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Person> GetPeople()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SavePayment(Payment pmnt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SavePerson(Person pers)
+        {
+            people.Add(pers);
+        }
+    }
+}
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -8,8 +8,17 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestSavePerson()
         {
+            Program program = new Program();
+            Assert.ThrowsException<NullReferenceException>(() => program.SavePerson(null, null));
+            MyDataProvider myDataProvider = new MyDataProvider();
+            Assert.ThrowsException<NullReferenceException>(() => program.SavePerson(myDataProvider, null));
+            Person person = new Person(0, "Bob");
+            Assert.ThrowsException<NullReferenceException>(() => program.SavePerson(null, person));
+            program.SavePerson(myDataProvider, person);
+            Assert.AreEqual(1, myDataProvider.CountPeople());
+            Assert.ThrowsException<ArgumentException>(() => program.SavePerson(myDataProvider, person));
         }
     }
 }
